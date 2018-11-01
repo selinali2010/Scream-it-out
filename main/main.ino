05e0067fd3810321dc29573ddacae3726ba377bd
Hannah Li <hk4li@uwaterloo.ca>
2018-11-01 12:20:12 -0400

    Fix the replay mechanic and when timer begins, changed attempt at ignoring background noise

--- main/main.ino
+++ main/main.ino
@@ -23,6 +23,18 @@ byte smiley[8] = {
   B00000,
 };
 
+byte canister[8] = {
+  B01110,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
+
+int STARTUP = 0, READY = 1, SCREAM = 2, END = 3, REPLAY = 4;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -32,35 +44,87 @@ void setup() {
   display.print("hk4li and sk6li");
   display.createChar(0, bar);
   display.createChar(1, smiley);
+  display.createChar(2, canister);
   delay(1000);
 }
 
-int countBars = 0, sensorValue = 0, buttonValue = 0;
-int prvSensorValue = 0;
+int countBars = 0, sensorValue = 0, buttonValue = 0, state = STARTUP;
+int gauge = 0, cans = 0;
+unsigned long gameTime = 0;
 
 void loop() {
-  // put your main code here, to run repeatedly:
   buttonValue = digitalRead(4);
-  if(buttonValue == HIGH){
-    sensorValue = analogRead(A1);
-    Serial.println(sensorValue);
-    if(sensorValue >= 400){
-      digitalWrite(LED_BUILTIN, HIGH);
-    } else {
-      digitalWrite(LED_BUILTIN, LOW);
+  
+  if(state == STARTUP){
+    if(buttonValue == 1){
+       state = READY;
     }
-    display.setCursor(countBars,1);
-    if(prvSensorValue >= 400 && sensorValue >= 400 && countBars != -1){//buttonValue == HIGH && countBars != -1){
-      countBars++;
-      display.write(byte(0)); 
+  } else if(state == READY){
+    display.setCursor(0,0);
+    display.print("Click button and");
+    display.setCursor(0,1);
+    display.print("start screaming!");
+    cans = 0;
+    countBars = 0;
+    gauge = 0;
+    
+    if(buttonValue == 1){
+      state = SCREAM;
+      gameTime = millis();
+      display.clear();
+      display.write(byte(2));
+      display.print(cans);
     }
-    if(countBars == 15){
-      display.write(byte(1));
-      countBars = -1;
-    } 
-    prvSensorValue = sensorValue;
+  } else if(state == SCREAM){
+    if(millis() - gameTime > 30000){
+      state = END;
+    } else {// if(buttonValue == HIGH){
+      sensorValue = analogRead(A1);
+      if(sensorValue >= 300){
+        gauge += sensorValue - 300;
+      }
+      
+      Serial.println(sensorValue);
+      
+      display.setCursor(countBars,1);
+      if(gauge > 1000){
+        gauge -= 1000;
+        countBars++;
+        display.write(byte(0)); 
+      }
+
+      /*
+      display.setCursor(4,0);
+      display.print(gauge);
+      display.print("  ");
+      display.setCursor(countBars,1);
+      */
+      
+      if(countBars == 17){
+        cans++;
+        countBars = 0;
+        display.clear();
+        display.write(byte(2));
+        display.print(cans);
+        display.setCursor(0,1);
+        display.write(byte(0)); 
+      }
+    }
+  } else if(state == END){
+    display.clear();
+    display.write(byte(2));
+    display.print(cans);
+    display.setCursor(0,1);
+    display.print("Thnx 4 play'n!");
+    delay(5000);
+    state = REPLAY;
+  } else if(state == REPLAY){
+    display.setCursor(0,1);
+    display.print("Play again?     ");
+    if(buttonValue == 1){
+      state = READY;
+    }
   }
-  //display.clear();
-  //display.print(sensorValue);
-  delay(500);
+  
+  delay(100);
 }
