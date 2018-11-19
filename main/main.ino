162f2e46870932e2e6fcf6e784e48418e15b8297
Hannah Li <hk4li@uwaterloo.ca>
2018-11-18 20:14:42 -0500

    Add timer and reformat code

--- main/main.ino
+++ main/main.ino
@@ -34,6 +34,9 @@ byte canister[8] = {
 };
 
 int STARTUP = 0, READY = 1, SCREAM = 2, END = 3, REPLAY = 4;
+int FULLCAN = 750;
+unsigned long GAMELENGTH = 30000;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -48,9 +51,78 @@ void setup() {
   delay(1000);
 }
 
+/*
+Sulley
+Randall
+Joe Ranft
+Bud Luckey
+Noodles Rivera
+Bob Peterson
+Spike Jones
+George Sanderson
+Ricky Plesuski
+Lanky Schmidt
+Ted Pauley
+Claws Ward
+Harley Gerson
+*/
+
 int countBars = 0, sensorValue = 0, buttonValue = 0, state = STARTUP;
 int gauge = 0, cans = 0;
 unsigned long gameTime = 0;
+
+void printCans(int numCans){
+  display.setCursor(0,0);
+  display.write(byte(2));
+  display.print(numCans);
+}
+
+void clearBar(){
+  display.setCursor(0,1);
+  display.print("                ");
+}
+
+void updateRanking(int score){
+  display.setCursor(3, 0);
+  switch (score) {
+  case 0 ... 2:
+    Serial.print("B");
+    display.print("Beginner");
+    break;
+  case 3 ... 5:
+    Serial.print("T");
+    display.print(" Trainee");
+    break;
+  case 6 ... 8:
+    Serial.print("W");
+    display.print("  Worker");
+    break;
+   case 9 ... 12:
+    Serial.print("M");
+    display.print("  Master");
+    break;
+  default:
+    Serial.print("G");
+    display.print("  Genius");
+    break;
+  }
+}
+
+void printTime(int gameTime){
+  display.setCursor(13, 0);
+  int displayT = GAMELENGTH/60 - (millis() - gameTime)/60;
+  if(displayT <= 0){
+    display.print("  0");
+  } else if (displayT < 10){
+    display.print("  ");
+    display.print(displayT);
+  } else if(displayT < 99){
+    display.print(" ");    
+    display.print(displayT);
+  } else {
+    display.print(displayT); 
+  }
+}
 
 void loop() {
   buttonValue = digitalRead(4);
@@ -72,11 +144,11 @@ void loop() {
       state = SCREAM;
       gameTime = millis();
       display.clear();
-      display.write(byte(2));
-      display.print(cans);
+      printCans(cans);
+      updateRanking(cans);
     }
   } else if(state == SCREAM){
-    if(millis() - gameTime > 30000){
+    if(millis() - gameTime > GAMELENGTH){
       state = END;
     } else {// if(buttonValue == HIGH){
       sensorValue = analogRead(A1);
@@ -87,8 +159,8 @@ void loop() {
       Serial.println(sensorValue);
       
       display.setCursor(countBars,1);
-      if(gauge > 1000){
-        gauge -= 1000;
+      if(gauge > FULLCAN){
+        gauge -= FULLCAN;
         countBars++;
         display.write(byte(0)); 
       }
@@ -103,17 +175,17 @@ void loop() {
       if(countBars == 17){
         cans++;
         countBars = 0;
-        display.clear();
-        display.write(byte(2));
-        display.print(cans);
-        display.setCursor(0,1);
-        display.write(byte(0)); 
+        printCans(cans);
+        updateRanking(cans);
+        clearBar();
       }
+
+      printTime(gameTime);
     }
   } else if(state == END){
-    display.clear();
-    display.write(byte(2));
-    display.print(cans);
+    printCans(cans);
+    updateRanking(cans);
+    printTime(gameTime);
     display.setCursor(0,1);
     display.print("Thnx 4 play'n!");
     delay(5000);
